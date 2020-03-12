421436eb23d74711727a9d60f5dbec32a1bce042
JoakimGustavssonn <joakimgustavsson1993@hotmail.com>
2020-03-12 21:13:21 +0100

    g
    g

--- JS/JStoggle.js
+++ JS/JStoggle.js
@@ -1,17 +1,25 @@
+
+
 const infoBtns = document.getElementsByClassName("js-btnInfoToggle");
 
 for (let i = 0; i < infoBtns.length; i++) {
   infoBtns[i].addEventListener("click", function() {
     const infoItem = this.nextElementSibling;
+
+    
+
     if (infoItem.classList.contains("is-open")) {
       infoItem.classList.remove("is-open");
-    } else {
+    }
+    else {
       const isOpenInfo = document.querySelector('.is-open');
       if(isOpenInfo) {
         isOpenInfo.classList.remove('is-open');
+      
       }
+      
       infoItem.classList.add("is-open");
     }
-  });
+   } );
 }
 
